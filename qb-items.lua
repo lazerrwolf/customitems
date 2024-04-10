b0cfec31e1fdbfa6336a75b45b3bc3af33a89932
lazerrwolf <142634910+lazerrwolf@users.noreply.github.com>
2024-04-09 23:43:01 -0500

    Add files via upload

---
+++ qb-items.lua
@@ -0,0 +1,9 @@
+-- Older QB-Core Items Template --
+
+["changeme"] 		 			 = {["name"] = "changeme",       	    	["label"] = "change me",	 				["weight"] = 150, 		["type"] = "item", 		["image"] = "changeme.png", 			["unique"] = false, 	["useable"] = true, 	["shouldClose"] = true,   ["combinable"] = nil,   ["description"] = "Change Me!"},
+
+
+
+-- Newer QB Items Template --
+
+change me                       = { name = 'change_me', label = 'changeme', weight = 100, type = 'item', image = 'changeme.png', unique = true, useable = true, shouldClose = true, combinable = nil, description =  'change me!' },
